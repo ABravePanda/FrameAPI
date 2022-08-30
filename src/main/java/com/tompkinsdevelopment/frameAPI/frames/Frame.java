c69444c67895ba0e3acd4c8ffb19da61ffa2d55e
nichrocks1 <nichrocks1@gmail.com>
2022-08-30 15:10:23 -0400

    Test commit for discord

--- src/main/java/com/tompkinsdevelopment/frameAPI/frames/Frame.java
+++ src/main/java/com/tompkinsdevelopment/frameAPI/frames/Frame.java
@@ -4,7 +4,15 @@ import org.bukkit.inventory.Inventory;
 
 public interface Frame {
 
+    int getSize();
+    int getUsableSize();
     String getTitle();
     Inventory getInventory();
+    Inventory createInventory();
+    void registerItems();
+    void registerFrame();
+    void generateFrame();
+    void clearUsableItems();
+
 
 }
